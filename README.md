59128e6426a547194e533d86249f33f9573a4b8c
zsun-Jason <sz1118@hotmail.com>
2022-01-14 16:59:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-7vtkyf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-7vtkyf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
+
+@NgModule({
+  declarations: [AppComponent, MyCounterComponent],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ count: counterReducer })
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
